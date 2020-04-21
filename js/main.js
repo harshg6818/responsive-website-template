c43885a9cb4078a8b4677c5aab99f52e269fba2f
Harsh Gupta <harshg6818@gmail.com>
2020-04-21 15:23:21 +0530

    adding project

---
+++ js/main.js
@@ -0,0 +1,190 @@
+(function($) {
+    "use strict";
+
+    // Preloader (if the #preloader div exists)
+    $(window).on('load', function() {
+        if ($('#preloader').length) {
+            $('#preloader').delay(100).fadeOut('slow', function() {
+                $(this).remove();
+            });
+        }
+    });
+
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    // Initiate the wowjs animation library
+    new WOW().init();
+
+    // Header scroll class
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#header').addClass('header-scrolled');
+        } else {
+            $('#header').removeClass('header-scrolled');
+        }
+    });
+
+    if ($(window).scrollTop() > 100) {
+        $('#header').addClass('header-scrolled');
+    }
+
+    // Smooth scroll for the navigation and links with .scrollto classes
+    $('.main-nav a, .mobile-nav a, .scrollto').on('click', function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            if (target.length) {
+                var top_space = 0;
+
+                if ($('#header').length) {
+                    top_space = $('#header').outerHeight();
+
+                    if (!$('#header').hasClass('header-scrolled')) {
+                        top_space = top_space - 20;
+                    }
+                }
+
+                $('html, body').animate({
+                    scrollTop: target.offset().top - top_space
+                }, 1500, 'easeInOutExpo');
+
+                if ($(this).parents('.main-nav, .mobile-nav').length) {
+                    $('.main-nav .active, .mobile-nav .active').removeClass('active');
+                    $(this).closest('li').addClass('active');
+                }
+
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+                    $('.mobile-nav-overly').fadeOut();
+                }
+                return false;
+            }
+        }
+    });
+
+    // Navigation active state on scroll
+    var nav_sections = $('section');
+    var main_nav = $('.main-nav, .mobile-nav');
+    var main_nav_height = $('#header').outerHeight();
+
+    $(window).on('scroll', function() {
+        var cur_pos = $(this).scrollTop();
+
+        nav_sections.each(function() {
+            var top = $(this).offset().top - main_nav_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                main_nav.find('li').removeClass('active');
+                main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+            }
+        });
+    });
+
+    // jQuery counterUp (used in Whu Us section)
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    // Porfolio isotope and filter
+    $(window).on('load', function() {
+        var portfolioIsotope = $('.portfolio-container').isotope({
+            itemSelector: '.portfolio-item'
+        });
+        $('#portfolio-flters li').on('click', function() {
+            $("#portfolio-flters li").removeClass('filter-active');
+            $(this).addClass('filter-active');
+
+            portfolioIsotope.isotope({
+                filter: $(this).data('filter')
+            });
+        });
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+        $('.venobox').venobox();
+    });
+
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+
+    $(".intro-carousel").owlCarousel({
+        autoplay: false,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+})(jQuery);
+
+jQuery(document).ready(function($) {
+
+    var feedbackSlider = $('.feedback-slider');
+    feedbackSlider.owlCarousel({
+        items: 1,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        loop: true,
+        mouseDrag: true,
+        touchDrag: true,
+        navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+        responsive: {
+
+            // breakpoint from 767 up
+            767: {
+                nav: true,
+                dots: false
+            }
+        }
+    });
+
+    feedbackSlider.on("translate.owl.carousel", function() {
+        $(".feedback-slider-item h3").removeClass("animated fadeIn").css("opacity", "0");
+        $(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating").removeClass("animated zoomIn").css("opacity", "0");
+    });
+
+    feedbackSlider.on("translated.owl.carousel", function() {
+        $(".feedback-slider-item h3").addClass("animated fadeIn").css("opacity", "1");
+        $(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating").addClass("animated zoomIn").css("opacity", "1");
+    });
+    feedbackSlider.on('changed.owl.carousel', function(property) {
+        var current = property.item.index;
+        var prevThumb = $(property.target).find(".owl-item").eq(current).prev().find("img").attr('src');
+        var nextThumb = $(property.target).find(".owl-item").eq(current).next().find("img").attr('src');
+        var prevRating = $(property.target).find(".owl-item").eq(current).prev().find('span').attr('data-rating');
+        var nextRating = $(property.target).find(".owl-item").eq(current).next().find('span').attr('data-rating');
+        $('.thumb-prev').find('img').attr('src', prevThumb);
+        $('.thumb-next').find('img').attr('src', nextThumb);
+        $('.thumb-prev').find('span').next().html(prevRating + '<i class="fa fa-star"></i>');
+        $('.thumb-next').find('span').next().html(nextRating + '<i class="fa fa-star"></i>');
+    });
+    $('.thumb-next').on('click', function() {
+        feedbackSlider.trigger('next.owl.carousel', [300]);
+        return false;
+    });
+    $('.thumb-prev').on('click', function() {
+        feedbackSlider.trigger('prev.owl.carousel', [300]);
+        return false;
+    });
+
+}); //end ready
